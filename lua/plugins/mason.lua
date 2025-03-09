776272335a91032382ba8a556fe4b2f4bffdca6c
sunny bharne <sunnybharne@sunnys-Mac-mini.local>
2025-03-09 14:02:40 +0200

    updated mason config

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,9 @@
 return {
   "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "jay-babu/mason-null-ls.nvim",
+  },
   config = function()
     require("mason").setup({
       ui = {
@@ -10,7 +14,47 @@ return {
         },
       }
     })
+
+    -- require("mason-lspconfig").setup({
+    --   ensure_installed = {
+    --     "autotools-language-server",
+    --     "azure-pipelines-language-server",
+    --     "bash-language-server",
+    --     "css-lsp",
+    --     "docker-compose-language-service",
+    --     "dockerfile-language-server",
+    --     "eslint-lsp",
+    --     "grammarly-languageserver",
+    --     "html-lsp",
+    --     "java-language-server",
+    --     "json-lsp",
+    --     "lua-language-server",
+    --     "marksman",
+    --     "powershell-editor-services",
+    --     "prettier",
+    --     "pyright",
+    --     "robotframework-lsp",
+    --     "sonarlint-language-server",
+    --     "tailwindcss-language-server",
+    --     "terraform-ls",
+    --     "typescript-language-server",
+    --     "vim-language-server",
+    --   }
+    -- })
+    --
+    -- require("mason-null-ls").setup({
+    --   ensure_installed = {
+    --     "prettier",
+    --     "stylua",
+    --     "markdownlint",
+    --     "yamllint",
+    --     "textlint",
+    --     "tflint",
+    --     "tfsec",
+    --     "eslint_d",
+    --     "trivy",
+    --     "typos",
+    --   }
+    -- })
   end
 }
-
-
