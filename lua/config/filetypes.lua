974cd670baeacf3581cf35c858bea13ad440994a
sunny bharne <sunnybharne@sunnys-Mac-mini.local>
2025-03-22 10:29:48 +0200

    Added docker filetype

--- lua/config/filetypes.lua
+++ lua/config/filetypes.lua
@@ -1,7 +1,19 @@
+-- Add custom filetype detection rules in Neovim
+
+-- Define filetypes based on file extensions
 vim.filetype.add({
   extension = {
-    tf = "terraform",
-    tfvars = "terraform", -- Optional: Also fix .tfvars files
+    tf = "terraform", -- Files with .tf extension are recognized as Terraform
+    tfvars = "terraform", -- Files with .tfvars extension are also recognized as Terraform
   }
 })
 
+-- Define filetypes based on filenames and patterns
+vim.filetype.add({
+  filename = {
+    ["Dockerfile"] = "dockerfile", -- Files named 'Dockerfile' are recognized as Dockerfile
+  },
+  pattern = {
+    ["Dockerfile%..*"] = "dockerfile", -- Files matching 'Dockerfile.<something>' are recognized as Dockerfile
+  },
+})
--- lua/plugins/devcontainer.lua
+++ lua/plugins/devcontainer.lua
@@ -1,7 +1,27 @@
 return {
   'https://codeberg.org/esensar/nvim-dev-container',
-  dependencies = 'nvim-treesitter/nvim-treesitter',
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    'akinsho/toggleterm.nvim',
+  },
   config = function()
     require("devcontainer").setup{}
+
+    -- Keybinding to open a floating terminal attached to the dev container
+    -- vim.api.nvim_set_keymap('n', '<leader>dp', ':lua OpenDevContainerTerminal()<CR>', { noremap = true, silent = true })
+
+    -- Function to open a floating terminal
+    function OpenDevContainerTerminal()
+      local Terminal = require('toggleterm.terminal').Terminal
+      local devcontainer_terminal = Terminal:new({
+        cmd = "DevcontainerAttach bash",
+        direction = "float",
+        float_opts = {
+          border = "curved",
+        },
+        close_on_exit = true,
+      })
+      devcontainer_terminal:toggle()
+    end
   end
 }
